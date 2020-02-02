293dc0fa4b774928d2a8deb3119ffc9c39536c83
Boon Yuen <tan_boon_yuen@ichat.sp.edu.sg>
2020-02-02 21:54:45 +0800

    using Iterator with JsonObject keys

--- app/src/main/java/edu/sp/p0123456/mappjsonobjectiterator/MainActivity.java
+++ app/src/main/java/edu/sp/p0123456/mappjsonobjectiterator/MainActivity.java
@@ -3,13 +3,52 @@ package edu.sp.p0123456.mappjsonobjectiterator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = "MainActivity";
+    // sample json string to demo
+    String jsonStr = "{'id01':{'name':'ABC', 'price':123}, " +
+            "'id02':{'name':'XYZ', 'price':987}," +
+            "'id03':{'name':'DEF', 'price':555}" +
+            "}";
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        // get UI to display text
+        tv = findViewById(R.id.mytext);
+
+        // parse the sample json string
+        parseJson(jsonStr);
+    }
+
+    void parseJson(String s){
+        try {
+            JSONObject j = new JSONObject(s);
+            Log.d(TAG, j.toString());
+            // iterate using keys
+            Iterator<String> iter = j.keys();
+            String txt = "";
+            while(iter.hasNext()){
+                String key = iter.next();
+                JSONObject obj = j.getJSONObject(key);
+                //Log.d(TAG, obj.toString());
+                txt = txt + key  + " - " +obj.getString("name") +
+                        " @ $" + obj.getInt("price") +"\n";
+            }
+            // display result in UI
+            tv.setText(txt);
+        }
+        catch(Exception ex){
+            Log.e(TAG, ex.getMessage());
+        }
     }
 }
