997c499b3c794f158560b1243847892ce028085c
Boon Yuen <tan_boon_yuen@ichat.sp.edu.sg>
2020-02-02 21:19:04 +0800

    Initial commit

---
+++ app/src/main/java/edu/sp/p0123456/mappjsonobjectiterator/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.sp.p0123456.mappjsonobjectiterator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
